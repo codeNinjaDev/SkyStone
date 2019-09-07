002b83292044313d8a7be4501768dc69904fa2b2
Peter Chacko <workpraytoday@gmail.com>
2019-09-07 12:38:49 -0500

    base structure

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DummySubsystem.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class DummySubsystem implements Subsystem {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public void update(Telemetry telemetry) {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
