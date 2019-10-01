0d3d52cde0d907f242b72fd9c531ee8575af1ce0
Peter Chacko <workpraytoday@gmail.com>
2019-10-01 16:19:42 -0500

    added mecanum and field oriented functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
@@ -9,6 +9,10 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.Parameters;
+
+import org.firstinspires.ftc.teamcode.libs.DriveUtils;
+import org.firstinspires.ftc.teamcode.libs.Vector2D;
+
 /**
  * Created by peter on 11/22/17.
  */
@@ -16,14 +20,19 @@ import org.firstinspires.ftc.teamcode.Parameters;
 public class DriveSubsystem implements Subsystem {
     private Gamepad driverGamepad;
     double MAX_SPEED = 1;
+    private double m_rightSideInvertMultiplier = -1.0;
 
     private Telemetry tl;
     public HardwareMap hardwareMap = null;
 
     public DcMotor backLeftMotor = null;
-    DcMotor backRightMotor = null;
+    public DcMotor backRightMotor = null;
+
+    public DcMotor frontLeftMotor = null;
+    public DcMotor frontRightMotor = null;
     ElapsedTime pidTimer;
 
+    //TODO initialize frontMOtors
     public DriveSubsystem(HardwareMap hardwareMap, Gamepad driverGamepad, Telemetry tl) {
         pidTimer = new ElapsedTime();
         this.driverGamepad = driverGamepad;
@@ -71,8 +80,66 @@ public class DriveSubsystem implements Subsystem {
         setLeftDrive(Math.abs(left)*left*MAX_SPEED);
         setRightDrive(Math.abs(right)*right*MAX_SPEED);
     }
+
+    /**
+     * Drive method for Mecanum platform.
+     *
+     * <p>Angles are measured clockwise from the positive X axis. The robot's speed is independent
+     * from its angle or rotation rate.
+     *
+     * @param ySpeed    The robot's speed along the Y axis [-1.0..1.0]. Right is positive.
+     * @param xSpeed    The robot's speed along the X axis [-1.0..1.0]. Forward is positive.
+     * @param zRotation The robot's rotation rate around the Z axis [-1.0..1.0]. Clockwise is
+     *                  positive.
+     * @param gyroAngle The current angle reading from the gyro in degrees around the Z axis. Use
+     *                  this to implement field-oriented controls.
+     */
+    public void driveCartesian(double ySpeed, double xSpeed, double zRotation, double gyroAngle) {
+        ySpeed = DriveUtils.clamp(ySpeed, -1.0, 1.0);
+        ySpeed = DriveUtils.applyDeadband(ySpeed, 0.01);
+
+        xSpeed = DriveUtils.clamp(xSpeed, -1.0, 1.0);
+        xSpeed = DriveUtils.applyDeadband(xSpeed, 0.01);
+
+        Vector2D input = new Vector2D(ySpeed, xSpeed);
+        input.rotate(-gyroAngle);
+
+        double[] wheelSpeeds = new double[4];
+        wheelSpeeds[0] = input.x + input.y + zRotation;
+        wheelSpeeds[1] = -input.x + input.y - zRotation;
+        wheelSpeeds[2] = -input.x + input.y + zRotation;
+        wheelSpeeds[3] = input.x + input.y - zRotation;
+
+        DriveUtils.normalize(wheelSpeeds);
+
+        frontLeftMotor.setPower(wheelSpeeds[0]);
+        frontRightMotor.setPower(wheelSpeeds[1] * m_rightSideInvertMultiplier);
+        backLeftMotor.setPower(wheelSpeeds[2]);
+        backRightMotor.setPower(wheelSpeeds[0]  * m_rightSideInvertMultiplier);
+
+
+    }
+
+    /**
+     * Drive method for Mecanum platform.
+     *
+     * <p>Angles are measured counter-clockwise from straight ahead. The speed at which the robot
+     * drives (translation) is independent from its angle or rotation rate.
+     *
+     * @param magnitude The robot's speed at a given angle [-1.0..1.0]. Forward is positive.
+     * @param angle     The angle around the Z axis at which the robot drives in degrees [-180..180].
+     * @param zRotation The robot's rotation rate around the Z axis [-1.0..1.0]. Clockwise is
+     *                  positive.
+     */
+    public void drivePolar(double magnitude, double angle, double zRotation) {
+
+        driveCartesian(magnitude * Math.sin(angle * (Math.PI / 180.0)),
+                magnitude * Math.cos(angle * (Math.PI / 180.0)), zRotation, 0.0);
+    }
+
     public void tankDrive(double left, double right) {
-                if(left > 1) {
+
+        if(left > 1) {
             left = 1;
         } else if(left < -1) {
             left = -1;
@@ -148,6 +215,7 @@ public class DriveSubsystem implements Subsystem {
         stopDriving();
         reset();
     }
+
 
     // Computes the current battery voltage
     public double getBatteryVoltage() {
